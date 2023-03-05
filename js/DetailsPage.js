25edee3139d4253f79c06ddbd4c973e1bf6ae592
ahmedabdelrahim123 <aahmed.mabdelrahim60@gmail.com>
2023-03-05 21:12:51 +0200

    modification

--- js/DetailsPage.js
+++ js/DetailsPage.js
@@ -9,7 +9,7 @@ var item =[];
 async function getprevious(){
    var response=await fetch ("https://dummyjson.com/products");
    var data = await response.json();
-   var product=localStorage.getItem("show-item")
+   var product=localStorage.getItem("productDetails")
    product=JSON.parse(product)
    // console.log(product.id);
    var index= (product.id)-1;
@@ -17,20 +17,20 @@ async function getprevious(){
    if (pageindex !=0)
   { 
    var previousimage=data.products[index].images[pageindex--];
-   console.log(pageindex);
-   console.log(previousimage)
+  //  console.log(pageindex);
+  //  console.log(previousimage)
    usersimg.setAttribute("src",previousimage);}
 
 }
 async function getnext(){
    var response=await fetch ("https://dummyjson.com/products");
    var data = await response.json();
-   var product=localStorage.getItem("show-item")
+   var product=localStorage.getItem("productDetails")
    product=JSON.parse(product)
    // console.log(product.id);
    var index= (product.id)-1;
    // console.log(data.products[0].images[pageindex++])
-   if (pageindex !=4)
+   if (pageindex !=3)
    { 
       var nextimage=data.products[index].images[pageindex++];
       console.log(pageindex);
@@ -40,55 +40,63 @@ async function getnext(){
    
 }
 
- async function purchase(){
-   var response=await fetch ("https://dummyjson.com/products");
-    var data = await response.json();
-    var product=localStorage.getItem("show-item")
-    product=JSON.parse(product)
-    console.log(product.id);
-    var index= (product.id)-1;
-    var temp= {id:data.products[index].id,
-      brand:data.products[index].brand,
-      category:data.products[index].category,
-      description:data.products[index].description,
-      title:data.products[index].title,
-      quantity:i }
-      console.log("temp");
-    console.log( temp);
-    localStorage.setItem("show-item",JSON.stringify(temp))
-   // var product=localStorage.getItem("show-item")
-   //  console.log(product);  
-}
+//  async function purchase(){
+//    var response=await fetch ("https://dummyjson.com/products");
+//     var data = await response.json();
+//     var product=localStorage.getItem("show-item")
+//     product=JSON.parse(product)
+//     console.log(product.id);
+//     var index= (product.id)-1;
+//     var temp= {id:data.products[index].id,
+//       brand:data.products[index].brand,
+//       category:data.products[index].category,
+//       description:data.products[index].description,
+//       title:data.products[index].title,
+//       quantity:i }
+//       console.log("temp");
+//     console.log( temp);
+//     localStorage.setItem("show-item",JSON.stringify(temp))
+//    // var product=localStorage.getItem("show-item")
+//    //  console.log(product);  
+// }
 async function chart() {
   var response = await fetch("https://dummyjson.com/products");
   var data = await response.json();
-  var product = localStorage.getItem("show-item");
+  var product = localStorage.getItem("productDetails");
+  var logineduser= localStorage.getItem("login");
   product = JSON.parse(product);
-  console.log(product.id);
+  console.log("login ls")
+  // console.log(product.cart);
   var index = product.id - 1;
   var temp = {
-    id: data.products[index].id,
-    brand: data.products[index].brand,
-    category: data.products[index].category,
-    description: data.products[index].description,
+    // id: data.products[index].id,
+    // brand: data.products[index].brand,
+    price: data.products[index].price,
     title: data.products[index].title,
-    quantity: i,
+    quantity: i
   };
-  item.push(temp);
-  localStorage.setItem("add-to-cart", JSON.stringify(item));
-  console.log("item");
-  console.log(item);
+  console.log(temp)
+  //  logineduser.cart.push(temp);
+   localStorage.setItem("login", JSON.stringify(logineduser.cart));
+   console.log(users)
+
+  // if (product[index].quantity !=0)
+  // {item.push(product[index]);
+  // localStorage.setItem("users", JSON.stringify(item));
+  // console.log(users)}
+
+  // console.log("item");
+  // console.log(item);
+
+  const box = document.createElement("div");
+  box.setAttribute("class",`alert alert-success`);
+box.innerHTML = 'sucessfly added to chart';
+console.log(box);
+document.body.appendChild(box);
 }
+
 async function back (){
-   var response=await fetch ("https://dummyjson.com/products");
-   var data = await response.json();
-   var product=localStorage.getItem("show-item")
-   product=JSON.parse(product)
-   // if (product.quantity===0)
-   // {   
-      
-   //    localStorage.removeItem("show-item")
-   //    }
+window.location.href="home.html";
 }
 
 function increase() {
@@ -106,35 +114,20 @@ function decrease() {
 async function displaydata() {
   var response = await fetch("https://dummyjson.com/products");
   var data = await response.json();
-  // console.log(data);
-
   //local storage
-  var product = localStorage.getItem("show-item");
-  product = JSON.parse(product);
-  // var index= (product.id)-1;
-  // console.log(index);
+  var product = localStorage.getItem("productDetails");
+  // console.log(product);
 
-usersimg.setAttribute("src",data.products[index].images[pageindex]);
-userstitle.innerHTML=`${data.products[0].title}`;
-usersprice.innerHTML=`${data.products[0].price}`;
+  products = JSON.parse(product);
+  console.log(products);
+  var index= (products.id)-1;
+  console.log(index);
+
+usersimg.setAttribute("src",data.products[index].images[0]);
+userstitle.innerHTML=`${data.products[index].title}`;
+usersprice.innerHTML=`${data.products[index].price}`;
 
   // console.log(data.products[0]);
-
-var arrofobj=Object.values(data.products[0])
-var index=0;
-var temp= {id:data.products[index].id,
-   brand:data.products[index].brand,
-   category:data.products[index].category,
-   description:data.products[index].description,
-   title:data.products[index].title,
-   quantity:i ,
-   
-    images:data.products[index].images
-  
-}
-   console.log("temp");
- console.log( temp);
- localStorage.setItem("show-item",JSON.stringify(temp))
 }
 // displaydata().then(()=>{
 
