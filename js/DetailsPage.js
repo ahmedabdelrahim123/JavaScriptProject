f00e92b29e699e205d8148c255570b62f22f6d92
ahmedabdelrahim123 <aahmed.mabdelrahim60@gmail.com>
2023-03-06 21:01:43 +0200

    uu

--- js/DetailsPage.js
+++ js/DetailsPage.js
@@ -22,9 +22,10 @@ async function getprevious(){
    if (pageindex !=0)
   { 
    var previousimage=data.products[index].images[pageindex--];
-  //  console.log(pageindex);
-  //  console.log(previousimage)
-   usersimg.setAttribute("src",previousimage);}
+   console.log(pageindex);
+   console.log(previousimage);
+   usersimg.setAttribute("src",previousimage);
+  }
 
 }
 async function getnext(){
@@ -35,7 +36,7 @@ async function getnext(){
    // console.log(product.id);
    var index= (product.id)-1;
    // console.log(data.products[0].images[pageindex++])
-   if (pageindex !=4)
+   if (pageindex !=3)
    { 
       var nextimage=data.products[index].images[pageindex++];
       console.log(pageindex);
@@ -44,70 +45,124 @@ async function getnext(){
 
    
 }
-async function chart() {
+
+var fetchedData;
+async function getData() {
   var response = await fetch("https://dummyjson.com/products");
-  var data = await response.json();
-  var items = localStorage.getItem("productDetails");
-  var productts = localStorage.getItem("allProducts");
-  var currentUser= localStorage.getItem("login");
-  var userrs= localStorage.getItem("users");
-  
-  item = JSON.parse(items);
-  currentUser = JSON.parse(currentUser);
-  users = JSON.parse(userrs);
-  products = JSON.parse(productts);
- console.log(products[(item.id)-1]);
+  fetchedData = await response.json();
+  return fetchedData.products;
+}
 
 
+function chart() {
+  // console.log("hello");
+  var users = JSON.parse(localStorage.getItem("users"));
+  var currentUser = JSON.parse(localStorage.getItem("login"));
+  var productId = this.id;
+  for (item of data) {
+    if (productId == item.id) {
+      item.quantity = i;
+      cartProduct = item;
+    }
+  }
   if (localStorage.getItem("login") != null) {
-    
     for (var user of users) {
       if (user.email == currentUser.email) {
-      // if (item.id==user.id)
-        // console.log(item);
- if (i!=0){
+        var result = user.cart.find((item) => item.id == productId);
+         console.log(result)
+        if (result == undefined) {
+          user.cart.push(cartProduct);
+          console.log(users);
 
-var flag=0;
-  for(var n=0; n<user.cart.length;n++){
-    if(user.cart[n].id==item.id ){
-      flag=1
-      break;
-    }
-    }
-
-  if(flag==0){
-    cartProduct = {
-      id: item.id,
-      title: products[(item.id)-1].title,
-      category: products[(item.id)-1].category,
-      price: products[(item.id)-1].price,
-      image: products[(item.id)-1].images,
-      quantity: i,
-      
-    };
-    user.cart.push(cartProduct)
-      localStorage.setItem("users", JSON.stringify(users));
-  }
-  else{
-    alert("this item is already added with same quantity before");
-    flag=0;
-  }
-
-  
-}else{
-  $(specify_quantity).modal("show");
-}
+          localStorage.setItem("users", JSON.stringify(users));
+        } else {
+        }
       }
     }
   } else {
     $(login).modal("show");
   }
-
-
+  this.disabled = true;
 }
-// chart().then(()=>{
-//   avoidmultipleaddition();
-// })
+
+
+
+// async function chart() {
+//   var response = await fetch("https://dummyjson.com/products");
+//   var data = await response.json();
+//   var items = localStorage.getItem("productDetails");
+//   var productts = localStorage.getItem("allProducts");
+//   var currentUser= localStorage.getItem("login");
+//   var userrs= localStorage.getItem("users");
+  
+//   item = JSON.parse(items);
+//   currentUser = JSON.parse(currentUser);
+//   users = JSON.parse(userrs);
+//   products = JSON.parse(productts);
+//  console.log(products[(item.id)-1]);
+
+
+//   if (localStorage.getItem("login") != null) {
+    
+//     for (var user of users) {
+//       if (user.email == currentUser.email) {
+//       // if (item.id==user.id)
+//         // console.log(item);
+//  if (i!=0){
+
+// var flag=0;
+// console.log(user.cart.length);
+//   for(var n=0; n<user.cart.length;n++){
+//     if(user.cart[n].id==item.id ){
+//       flag=1
+//       break;
+//     }
+//     }
+
+//   if(flag==0 ){
+//     cartProduct = {
+//       id: item.id,
+//       title: products[(item.id)-1].title,
+//       category: products[(item.id)-1].category,
+//       price: products[(item.id)-1].price,
+//       image: products[(item.id)-1].images,
+//       quantity: i,
+      
+//     };
+//     user.cart.push(cartProduct)
+//       localStorage.setItem("users", JSON.stringify(users));
+//   }
+//   else{
+//     if (i !=user.cart[n].quantity){
+//       cartProduct = {
+//         id: item.id,
+//         title: products[(item.id)-1].title,
+//         category: products[(item.id)-1].category,
+//         price: products[(item.id)-1].price,
+//         image: products[(item.id)-1].images,
+//         quantity: i,
+        
+//       };
+//       user.cart[n]=cartProduct;
+//     }
+//     else {
+//       alert("this item is already added with same quantity before");
+//     flag=0;}
+//   }
+
+  
+// }else{
+//   $(specify_quantity).modal("show");
+// }
+//       }
+//     }
+//   } else {
+//     $(login).modal("show");
+//   }
+
+
+// }
+
 
 async function back (){
 window.location.href="home.html";
